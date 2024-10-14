1180689d35dedab82da651fecf72701a6f2250c7
Haley <haley.ward@lifeomic.com>
2024-10-14 08:59:04 -0400

    make small change for testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,8 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('renders a button', async ({ page }) => {
   await page.goto('/iframe.html?args=&id=button--primary&viewMode=story');
-
   await page.waitForSelector('glide-core-button');
-
   await expect(page).toHaveScreenshot(['button.test.png']);
 });
